1ea3c1b3f53d0c102d42e9862de65564d6b83821
david85258 <marcos14david7@gmail.com>
2023-05-05 21:22:49 +0200

    Plains Generation

--- src/main/java/net/david85258/rainbowbiomes/WorldGenerator/CustomChunkGenerator.java
+++ src/main/java/net/david85258/rainbowbiomes/WorldGenerator/CustomChunkGenerator.java
@@ -16,31 +16,78 @@ public class CustomChunkGenerator extends ChunkGenerator {
 
     @Override
     public void generateNoise(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {
+        super.generateNoise(worldInfo, random, chunkX, chunkZ, chunkData);
         for (int x = 0; x < 16; x++)
-            for (int z = 0; z < 16; z++) {
-                for (int y = -63; y <=0; y++){
-                    if (y < -3) chunkData.setBlock(x, y, z, Material.STONE);
-                    else chunkData.setBlock(x, y, z, Material.GRASS_BLOCK);
-                }
-                if (chunkX == 1 && x == 8){
-                    chunkData.setBlock(x, 1, z, Material.OAK_LEAVES);
-                } else if (chunkX == 2 && x == 8) {
-                    chunkData.setBlock(x, 1, z, Material.WATER);
-                }
+            for (int z = 0; z < 16; z++)
                 for (int y = worldInfo.getMinHeight(); y <= worldInfo.getMaxHeight(); y++){
                     ((CraftChunkData)chunkData).getHandle().setBiome(x, y, z, BiomeManager.registryWriteable.getHolderOrThrow(getKey((chunkX * 16) + x, (chunkZ * 16) + z)));
                 }
-            }
     }
 
     @Override
-    public void generateBedrock(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {
-        for (int x = 0; x < 16; x++)
-            for (int z = 0; z < 16; z++) {
-                chunkData.setBlock(x, -64, z, Material.BEDROCK);
-            }
+    public boolean shouldGenerateNoise() {
+        return true;
     }
 
+    @Override
+    public boolean shouldGenerateNoise(WorldInfo worldInfo, Random random, int chunkX, int chunkZ) {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateSurface() {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateSurface(WorldInfo worldInfo, Random random, int chunkX, int chunkZ) {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateBedrock() {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateCaves() {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateCaves(WorldInfo worldInfo, Random random, int chunkX, int chunkZ) {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateDecorations() {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateDecorations(WorldInfo worldInfo, Random random, int chunkX, int chunkZ) {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateMobs() {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateMobs(WorldInfo worldInfo, Random random, int chunkX, int chunkZ) {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateStructures() {
+        return true;
+    }
+
+    @Override
+    public boolean shouldGenerateStructures(WorldInfo worldInfo, Random random, int chunkX, int chunkZ) {
+        return true;
+    }
 
     private ResourceKey<Biome> getKey(int x, int z){
         ResourceLocation RESOURCE_LOCATION = new ResourceLocation("rainbowbiomes", a(Math.abs(circulo(x, z))+1)+"");
